553131c7576c40b0cbd88ac7e56f5e9ce155c5dc
Talha Satır <imnever0729@gmail.com>
2022-11-25 01:02:30 +0300

    ilk type script komutları yazıldı ve çalıştırıldı

---
+++ greeter.ts
@@ -0,0 +1,6 @@
+function greeter(name){
+    console.log('Hello '+ name)
+}
+
+let user = "Talha"; //let es6 ile geldiğinden js ye , var 'a dönüştürür. (es5 de versiyonu kullanıyor dönüştürücü ve o versiyonda let yoktu çünkü)
+ greeter(user);
